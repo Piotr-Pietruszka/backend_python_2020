90c28968a8be8432ef4fdc66b494fd583edee0c2
Piotr-Pietruszka <piopie98@wp.pl>
2020-08-03 15:24:38 +0200

    Najpopularniejsze miasta i hasla. Uzytkownicy urodzeni w danym zakresie. Obliczenie dni do urodzin

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import sqlite3
 import json
-from datetime import date
+from datetime import datetime, timedelta
 import re
 from peewee import *
-
-
 
 
 # connection to database
@@ -92,6 +90,29 @@ def gender_percentage():
     return 100.0*female_count/all_count, 100.0*male_count/all_count
 
 
+def most_popular_cities(n):
+    query = Location.select(Location.city, fn.COUNT(Location.city).alias('city_count')).group_by(Location.city).order_by(fn.COUNT(Location).desc()).limit(n)
+    for result in query:
+        print(result.city, result.city_count)
+
+
+def most_popular_passwords(n):
+    query = Login.select(Login.password, fn.COUNT(Login.password).alias('pass_count')).group_by(Login.password).order_by(fn.COUNT(Login).desc()).limit(n)
+    for result in query:
+        print(result.password, result.pass_count)
+
+
+def date_range(date_1, date_2):
+    date_time_1 = datetime.strptime(date_1, '%Y-%m-%d')
+    date_time_2 = datetime.strptime(date_2, '%Y-%m-%d')
+
+    print(f"{date_time_1} - {date_time_2}")
+    query = Person.select().where(Person.date_of_birth > date_time_1, Person.date_of_birth < date_time_2)
+    for result in query:
+        print(result.login.username)
+
+
+
 persons_db.connect()
 persons_db.create_tables([Person, Location, Login])
 
@@ -104,9 +125,35 @@ with open('persons.json', encoding='utf-8') as f:
 
                 # Changes before saving to database
                 del person['picture']
-                person['day_to_birthday'] = 9
                 person['phone'] = re.sub('[^0-9]', '', person['phone'])
                 person['cell'] = re.sub('[^0-9]', '', person['phone'])
+
+                # Changing dates to datetime type from string
+                dob_date_time = datetime.strptime(person['dob']['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
+                registered_date_time = datetime.strptime(person['registered']['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
+
+                # Calculating days to birthday
+                current_datetime = datetime.now()
+                birthday_date_time_temp = dob_date_time
+                try:
+                    birthday_date_time_temp = birthday_date_time_temp.replace(year=current_datetime.year)
+                except ValueError:
+                    # In case od leap year
+                    birthday_date_time_temp = birthday_date_time_temp.replace(year=current_datetime.year,
+                                                                              day=birthday_date_time_temp.day+1)
+                days_to_birthday = (birthday_date_time_temp.date() - current_datetime.date()).days
+                if days_to_birthday < 0:
+                    try:
+                        birthday_date_time_temp = birthday_date_time_temp.replace(year=current_datetime.year + 1)
+                    except ValueError:
+                        # In case od leap year
+                        birthday_date_time_temp = birthday_date_time_temp + timedelta(days=1)
+                        birthday_date_time_temp = birthday_date_time_temp.replace(year=current_datetime.year + 1)
+
+                    days_to_birthday = (birthday_date_time_temp.date() - current_datetime.date()).days
+
+                person['day_to_birthday'] = days_to_birthday
+
 
                 # Checking if person already exists (by username)
                 query = Login.select().where(Login.username == person['login']['username'])
@@ -136,8 +183,8 @@ with open('persons.json', encoding='utf-8') as f:
                     loc_temp_db.save()
                     p_temp_db = Person(gender=person['gender'], title=person['name']['title'],
                                           first_name=person['name']['first'], last_name=person['name']['last'], location=loc_temp_db,
-                                          email=person['email'], login=loc_temp_db, date_of_birth=person['dob']['date'],
-                                          age=person['dob']['age'], register_date=person['registered']['date'],
+                                          email=person['email'], login=loc_temp_db, date_of_birth=dob_date_time,
+                                          age=person['dob']['age'], register_date=registered_date_time,
                                           register_age=person['registered']['age'], phone=person['phone'],
                                           cell=person['cell'], id_name=person['id']['name'],
                                           id_value=person['id']['value'],
@@ -147,8 +194,18 @@ with open('persons.json', encoding='utf-8') as f:
 
 female_percentage, male_percentage = gender_percentage()
 
+print("\nCititees: ")#DEL
+most_popular_cities(5)
+print("\nPAsswords: ")#DEL
+most_popular_passwords(7)
+
+
+print("\nDAte range: ")#DEL
+date_range('1997-01-30', '1998-02-04')
 
 persons_db.close()
+
+
 
 
 
